065d3aadf2e515f4683dec2ab679a9c39a2b8425
stellamoore2107 <2024031@apps.nsd.org>
2025-05-20 16:32:46 +0000

    jksjafkl

---
+++ js.js
@@ -0,0 +1,12 @@
+
+var LIST = [
+    "frozen burritos ",
+    "banana foster",
+    "egg mcmuffin with egg"
+];
+
+for(let i = 0; i < LIST.length; i++){
+
+alert("frozen burritos: " + LIST[i]);
+
+}
